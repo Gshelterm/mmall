9e5e52a11158c18f4aa38a181dea910d5de087e6
Gshelterm <Gtms0001@163.com>
2018-04-02 22:17:42 +0800

    remove no used import

--- src/main/java/com/mmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -7,13 +7,9 @@ import com.mmall.dao.UserMapper;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
 import com.mmall.util.MD5Util;
-import com.sun.xml.internal.ws.server.ServerRtException;
-import net.sf.jsqlparser.schema.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
-import javax.servlet.http.HttpSession;
 import java.util.UUID;
 
 @Service
